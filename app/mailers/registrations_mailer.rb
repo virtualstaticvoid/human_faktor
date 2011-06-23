7da7953071e6fe8bb895f1194dcbec666b5d6382
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-23 17:55:53 +0200

    small typo

--- app/mailers/registrations_mailer.rb
+++ app/mailers/registrations_mailer.rb
@@ -2,7 +2,7 @@ class RegistrationsMailer < BaseMailer
 
   def completed(registration)  
     @registration = registration
-    mail(:to => registration.email, :subject => "Welcome To #{AppConfig.title} - Registration Complete")  
+    mail(:to => registration.email, :subject => "Welcome to #{AppConfig.title} - Registration Complete")  
   end  
 
 end
