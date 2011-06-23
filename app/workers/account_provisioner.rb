53a93ac54d9226b651180fb17d2da054a6351bb0
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-23 14:28:06 +0200

    updated account provisioner to send registration mailer

--- app/workers/account_provisioner.rb
+++ app/workers/account_provisioner.rb
@@ -2,11 +2,17 @@ class AccountProvisioner
   @queue = :account_provisioner_queue
   
   def self.perform(registration_id)
+  
     registration = Registration.find(registration_id)
     
     # TODO: create account etc...
     
+    # make the registration active
     registration.update_attribute(:active, true)
+
+    # send welcome email
+    RegistrationsMailer.completed(registration).deliver
+    
   end
   
 end
