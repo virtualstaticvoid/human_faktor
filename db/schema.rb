9b3b4fe8ebfe67539f43f8e75bc532f15a5ad059
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-23 08:48:46 +0200

    changed to use database based sessions

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(:version => 20110622101001) do
   add_index "registrations", ["identifier"], :name => "index_registrations_on_identifier", :unique => true
   add_index "registrations", ["subdomain"], :name => "index_registrations_on_subdomain", :unique => true
 
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "subscriptions", :force => true do |t|
     t.integer  "sequence",                                :null => false
     t.string   "title",                                   :null => false
