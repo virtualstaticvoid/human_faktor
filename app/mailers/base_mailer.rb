a6c85f7dc030224cbf6ab8521573e19d148c2821
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-25 18:59:33 +0200

    fix up for development mode

--- app/mailers/base_mailer.rb
+++ app/mailers/base_mailer.rb
@@ -1,5 +1,18 @@
 class BaseMailer < ActionMailer::Base
 
+  # configure mailer for SMTP in development
+  if Rails.env.development?
+    ActionMailer::Base.smtp_settings = {  
+      :address              => AppConfig.smtp_server,  
+      :port                 => AppConfig.smtp_port,  
+      :domain               => AppConfig.smtp_domain,  
+      :authentication       => :plain,  
+      :user_name            => AppConfig.smtp_user_name,  
+      :password             => AppConfig.smtp_password,  
+      :enable_starttls_auto => false
+    }
+  end
+
   ActionMailer::Base.default_url_options[:host] = AppConfig.domain
 
   default :from => AppConfig.no_reply_email
