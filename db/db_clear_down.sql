cf08675a6c99fd74c3cb107ff9c3e76d2acbadad
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-27 13:28:25 +0200

    added script for clearing down the database

---
+++ db/db_clear_down.sql
@@ -0,0 +1,7 @@
+﻿truncate public.calendar_entries;
+truncate public.countries;
+truncate public.partners;
+truncate public.subscriptions;
+truncate public.registrations;
+truncate public.sessions;
+
