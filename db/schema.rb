d71c5f8080902e4817c82879bc5d5d3f612560b1
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-27 15:44:42 +0200

    db schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,41 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110627105712) do
+ActiveRecord::Schema.define(:version => 20110627121437) do
+
+  create_table "account_subscriptions", :force => true do |t|
+    t.integer  "account_id"
+    t.date     "from_date",                             :null => false
+    t.date     "to_date",                               :null => false
+    t.string   "title",                                 :null => false
+    t.decimal  "price",                :default => 0.0, :null => false
+    t.integer  "max_employees",                         :null => false
+    t.integer  "threshold",            :default => 0,   :null => false
+    t.decimal  "price_over_threshold", :default => 0.0, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "account_subscriptions", ["account_id"], :name => "index_account_subscriptions_on_account_id"
+
+  create_table "accounts", :force => true do |t|
+    t.string   "identifier",                               :null => false
+    t.string   "subdomain",                                :null => false
+    t.string   "title",                                    :null => false
+    t.integer  "country_id",                               :null => false
+    t.integer  "partner_id"
+    t.string   "theme",             :default => "default", :null => false
+    t.integer  "fixed_daily_hours", :default => 8,         :null => false
+    t.boolean  "active",            :default => false,     :null => false
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "accounts", ["subdomain"], :name => "index_accounts_on_subdomain", :unique => true
 
   create_table "calendar_entries", :force => true do |t|
     t.integer  "country_id"
@@ -47,13 +81,13 @@ ActiveRecord::Schema.define(:version => 20110627105712) do
     t.string   "identifier",                         :null => false
     t.string   "subdomain",                          :null => false
     t.string   "title",                              :null => false
-    t.string   "name",                               :null => false
-    t.string   "email",                              :null => false
     t.integer  "country_id",                         :null => false
     t.integer  "subscription_id",                    :null => false
     t.integer  "partner_id"
-    t.string   "auth_token",                         :null => false
+    t.string   "name",                               :null => false
+    t.string   "email",                              :null => false
     t.boolean  "active",          :default => false, :null => false
+    t.string   "auth_token",                         :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
