6779dceee183b233174d63dda4c019b6e2d9df62
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-06-27 13:45:35 +0200

    correction to calendar entry model

--- app/models/calendar_entry.rb
+++ app/models/calendar_entry.rb
@@ -2,11 +2,11 @@ class CalendarEntry < ActiveRecord::Base
 
   default_scope order(:entry_date)
   
-  validates :country_id, :existence => true
+  belongs_to :country
+
+  validates :country, :presence => true, :existence => true
   validates :title, :presence => true
   validates :entry_date, :presence => true, :timeliness => { :type => :date }
-
-  belongs_to :country
   
   def to_s
     self.title
