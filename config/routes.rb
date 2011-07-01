b524d2217b3e2f9f5e2782b51b2f2c4a7988ffcf
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-02 01:50:01 +0200

    updated routes ~ refactored!

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,71 @@
-HumanFaktor::Application.routes.draw do
+HumanFaktor::Application.routes.drawdo
 
-  get "home/about"
-  get "home/contact"
-  get "home/features"
-  get "home/subscriptions"
-  get "home/terms"
-  get "home/partner"
-  get "home/sign_in"
+  ###
+  # routes for base
+  #
 
-  get "registrations/new", :to => 'registrations#new', :as => :new_account_registration  
-  post "registrations", :to => 'registrations#create', :as => :create_account_registration  
+  get "about", :to => 'home#about', :as => :home_about
+  get "contact", :to => 'home#contact', :as => :home_contact
+  get "features", :to => 'home#features', :as => :home_features
+  get "subscriptions", :to => 'home#subscriptions', :as => :home_subscriptions
+  get "terms", :to => 'home#terms', :as => :home_terms
+  get "partner", :to => 'home#partner', :as => :home_partner
+  get "sign_in", :to => 'home#sign_in', :as => :home_sign_in
+
+  get "registrations/new", :to => 'registrations#new', :as => :new_account_registration
+  post "registrations", :to => 'registrations#create', :as => :create_account_registration
   get "registrations/:id", :to => 'registrations#show', :as => :account_registration
   get "registrations/:id/query/:started(.:format)", :to => 'registrations#query', :as => :query_account_registration
 
-  devise_for :employees
-  # TODO: configuration for devise
+  ###
+  # routes for tenant
+  #
+
+  scope "*tenant" do
+
+    get "setup", :to => "tenant/account_setup#index", :as => :account_setup
+
+    devise_for :employees, :path => ""do
+      # TODO: additional configuration for devise
+    end
+
+    get "profile", :to => "tenant/dashboard#profile", :as => :profile
+    get "balance", :to => "tenant/dashboard#balance", :as => :balance
+    get "calendar", :to => "tenant/dashboard#calendar", :as => :calendar
+    get "leave", :to => "tenant/leave_requests#employee_index", :as => :leave_requests
+
+    get "staff_calendar", :to => "tenant/dashboard#staff_calendar", :as => :staff_calendar
+    get "staff_leave", :to => "tenant/leave_requests#staff_index", :as => :staff_leave_requests
+
+    get "leave/new", :to => "tenant/leave_requests#new", :as => :new_leave_request
+    post "leave", :to => "tenant/leave_requests#create", :as => :create_leave_request
+    get "leave/:id", :to => "tenant/leave_requests#edit", :as => :edit_leave_request
+    put "leave/:id/confirm", :to => "tenant/leave_requests#confirm", :as => :confirm_leave_request
+    put "leave/:id/approve", :to => "tenant/leave_requests#approve", :as => :approve_leave_request
+    put "leave/:id/decline", :to => "tenant/leave_requests#decline", :as => :decline_leave_request
+    put "leave/:id/cancel", :to => "tenant/leave_requests#cancel", :as => :cancel_leave_request
+
+    scope "account" do
+
+      get "edit", :to => "tenant/account#edit", :as => :edit_account
+      put "edit", :to => "tenant/account#update", :as => :update_account
+
+      get "policies", :to => "tenant/leave_types#edit", :as => :edit_leave_types
+      put "policies", :to => "tenant/leave_types#update", :as => :update_leave_types
+
+      resources :locations
+      resources :departments
+      resources :employees
+
+      get "/", :to => "tenant/account#index", :as => :account
+
+    end
+
+    get "/", :to => "tenant/dashboard#index", :as => :dashboard
+
+  end
 
   root :to => "home#index"
 
 end
+
