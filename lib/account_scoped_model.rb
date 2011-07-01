5d12b3297a190dd5ab6faeed8b9606c1552b5991
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-02 01:41:07 +0200

    revisions to account scoped model helper module

--- lib/account_scoped_model.rb
+++ lib/account_scoped_model.rb
@@ -8,12 +8,6 @@ module AccountScopedModel
 
         validates :account, :presence => true, :existence => true
 
-        before_create do
-          # TODO: assign account to the current account
-        end
-        
-        # TODO: ensure that the account has been assigned on validation...
-
       end
     end
   end
