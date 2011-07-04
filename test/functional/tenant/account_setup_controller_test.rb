25d9b1353f5aad0d995558aea95e1181a32270e4
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-04 08:33:56 +0200

    added functional test for account setup update action

--- test/functional/tenant/account_setup_controller_test.rb
+++ test/functional/tenant/account_setup_controller_test.rb
@@ -15,7 +15,35 @@ module Tenant
       assert_response :success
     end
     
-    # TODO
+    test "should update" do
+    
+      registration = @account.registration
+
+      account_setup = AccountSetup.new().tap do |setup|
+        setup.admin_first_name = registration.first_name
+        setup.admin_last_name = registration.last_name
+        setup.admin_user_name = registration.user_name
+        setup.admin_email = registration.email
+        setup.admin_password = 'sekret123'
+        setup.admin_password_confirmation = 'sekret123'
+
+        setup.fixed_daily_hours = @account.fixed_daily_hours
+        setup.leave_cycle_start_date = Date.new(Date.today.year, 1, 1)
+
+        LeaveType.for_each_leave_type do |leave_type_class|
+          leave_type_name = leave_type_class.name.gsub(/LeaveType::/, '').downcase
+
+          leave_type = @account.send("leave_type_#{leave_type_name}")
+          setup.send("#{leave_type_name}_leave_allowance=", leave_type.cycle_days_allowance)
+        end
+
+        setup.auth_token = @account.auth_token
+        setup.auth_token_confirmation = @account.auth_token
+      end
+    
+      put :update, :tenant => @account.subdomain, :account_setup => account_setup.attributes
+      assert_redirected_to dashboard_url(:tenant => @account.subdomain)
+    end
   
   end
 end
