41a8a7c792c6baec901f0ab5d09a721a22642978
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-06 13:29:04 +0200

    further work on leave requests controller test

--- test/functional/tenant/leave_requests_controller_test.rb
+++ test/functional/tenant/leave_requests_controller_test.rb
@@ -4,9 +4,10 @@ module Tenant
   class LeaveRequestsControllerTest < ActionController::TestCase
 
     setup do
-      @leave_request = leave_requests(:annual)
+      @leave_request = leave_requests(:one)
       @leave_request_attributes = @leave_request.attributes.merge!({ 
-        "identifier" => TokenHelper.friendly_token
+        "identifier" => TokenHelper.friendly_token,
+        "approver_comment" => 'Test comment by approver'
       })
     end
 
@@ -28,21 +29,24 @@ module Tenant
     end
 
     test "should get edit when status is pending" do
-      @leave_request.confirm
+      sign_in_as :employee
+      assert @leave_request.confirm
       assert @leave_request.status == LeaveRequest::STATUS_APPROVED
       get :edit, :tenant => @account.subdomain, :id => @leave_request.to_param
       assert_response :success
     end
 
     test "should get edit when status is declined" do
-      @leave_request.decline(employees(:admin), '')
+      sign_in_as :employee
+      assert @leave_request.decline(employees(:admin), '')
       assert @leave_request.status == LeaveRequest::STATUS_DECLINED
       get :edit, :tenant => @account.subdomain, :id => @leave_request.to_param
       assert_response :success
     end
 
     test "should get edit when status is cancelled" do
-      @leave_request.cancel
+      sign_in_as :employee
+      assert @leave_request.cancel
       assert @leave_request.status == LeaveRequest::STATUS_CANCELLED
       get :edit, :tenant => @account.subdomain, :id => @leave_request.to_param
       assert_response :success
@@ -52,37 +56,40 @@ module Tenant
 
       test "should approve for #{role}" do
         sign_in_as role
-        put :approve, :tenant => @account.subdomain, :id => @leave_request.to_param
+        put :approve, :tenant => @account.subdomain, :id => @leave_request.to_param, :leave_request => @leave_request_attributes
         assert_redirected_to dashboard_path(:tenant => @account.subdomain)
       end
 
       test "should decline for #{role}" do
         sign_in_as role
-        put :decline, :tenant => @account.subdomain, :id => @leave_request.to_param
+        put :decline, :tenant => @account.subdomain, :id => @leave_request.to_param, :leave_request => @leave_request_attributes
         assert_redirected_to dashboard_path(:tenant => @account.subdomain)
       end
 
       test "should cancel for #{role}" do
         sign_in_as role
-        put :cancel, :tenant => @account.subdomain, :id => @leave_request.to_param
+        put :cancel, :tenant => @account.subdomain, :id => @leave_request.to_param, :leave_request => @leave_request_attributes
         assert_redirected_to dashboard_path(:tenant => @account.subdomain)
       end
     
     end
 
     test "cannot approve for employee" do
-      put :approve, :tenant => @account.subdomain, :id => @leave_request.to_param
-      assert_response :failure
+      sign_in_as :employee
+      put :approve, :tenant => @account.subdomain, :id => @leave_request.to_param, :leave_request => @leave_request_attributes
+      assert_redirected_to dashboard_path(:tenant => @account.subdomain)
     end
 
     test "cannot decline for employee" do
-      put :decline, :tenant => @account.subdomain, :id => @leave_request.to_param
-      assert_response :failure
+      sign_in_as :employee
+      put :decline, :tenant => @account.subdomain, :id => @leave_request.to_param, :leave_request => @leave_request_attributes
+      assert_redirected_to dashboard_path(:tenant => @account.subdomain)
     end
 
     test "should cancel for employee" do
-      put :cancel, :tenant => @account.subdomain, :id => @leave_request.to_param
-      assert_response :failure
+      sign_in_as :employee
+      put :cancel, :tenant => @account.subdomain, :id => @leave_request.to_param, :leave_request => @leave_request_attributes
+      assert_redirected_to dashboard_path(:tenant => @account.subdomain)
     end
     
   end
