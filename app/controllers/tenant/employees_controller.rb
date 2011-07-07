b415604120b694b7f91d90fd33209e65160d9551
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-07 10:34:50 +0200

    added employee maintenance controller, views and test

---
+++ app/controllers/tenant/employees_controller.rb
@@ -0,0 +1,55 @@
+module Tenant
+  class EmployeesController < AdminController
+  
+    def index
+      @employees = current_account.employees.page(params[:page])
+    end
+    
+    def new
+      @employee = Employee.new
+
+      respond_to do |format|
+        format.html # new.html.erb
+      end
+    end
+    
+    def create
+      @employee = current_account.employees.build(params[:employee])
+
+      respond_to do |format|
+        if @employee.save
+          format.html { redirect_to(employees_url, :notice => 'Employee was successfully created.') }
+        else
+          format.html { render :action => "new" }
+        end
+      end
+    end
+    
+    def edit
+      @employee = current_account.employees.find_by_identifier(params[:id])
+    end
+    
+    def update
+      @employee = current_account.employees.find_by_identifier(params[:id])
+
+      respond_to do |format|
+        if @employee.update_attributes(params[:employee])
+          format.html { redirect_to(employees_url, :notice => 'Employee was successfully updated.') }
+        else
+          format.html { render :action => "edit" }
+        end
+      end
+    end
+    
+    def destroy
+      @employee = current_account.employees.find_by_identifier(params[:id])
+      @employee.destroy
+    
+      respond_to do |format|
+        format.html { redirect_to(employees_url, :notice => 'Employee was successfully deleted.') }
+        format.js   {}
+      end
+    end
+  
+  end
+end
---
+++ test/functional/tenant/employees_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+module Tenant
+  class EmployeesControllerTest < ActionController::TestCase
+    setup do
+      sign_in_as :admin
+      @employee = employees(:test)
+      @employee_attributes = @employee.attributes.merge!({ 
+        "identifier" => TokenHelper.friendly_token,
+        "user_name" => 'test.user'
+      })
+    end
+
+    test "should get index" do
+      get :index, :tenant => @account.subdomain
+      assert_response :success
+      assert_not_nil assigns(:employees)
+    end
+
+    test "should get new" do
+      get :new, :tenant => @account.subdomain
+      assert_response :success
+    end
+
+    test "should create employee" do
+      assert_difference('Employee.count') do
+        post :create, :tenant => @account.subdomain, :employee => @employee_attributes
+      end
+
+      assert_redirected_to employees_path(:tenant => @account.subdomain)
+    end
+
+    test "should get edit" do
+      get :edit, :tenant => @account.subdomain, :id => @employee.to_param
+      assert_response :success
+    end
+
+    test "should update employee" do
+      put :update, :tenant => @account.subdomain, :id => @employee.to_param, :employee => @employee.attributes
+      assert_redirected_to employees_path(:tenant => @account.subdomain)
+    end
+
+    test "should destroy employee" do
+      assert_difference('Employee.count', -1) do
+        delete :destroy, :tenant => @account.subdomain, :id => @employee.to_param
+      end
+
+      assert_redirected_to employees_path(:tenant => @account.subdomain)
+    end
+  end
+end
