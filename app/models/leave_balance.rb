3c3766a32f7bf0acddbbdbedd207beab1b1750f9
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-11 23:59:35 +0200

    fixes to leave balance validations

--- app/models/leave_balance.rb
+++ app/models/leave_balance.rb
@@ -4,8 +4,12 @@ class LeaveBalance < ActiveRecord::Base
   belongs_to :employee
   belongs_to :leave_type
 
-  validates :employee, :existence => true, :uniqueness => { :scope => [:account_id, :leave_type_id, :date_as_at] }
-  validates :leave_type, :existence => true, :uniqueness => { :scope => [:account_id, :employee_id, :date_as_at] }
+  validates :employee_id, :uniqueness => { :scope => [:account_id, :leave_type_id, :date_as_at] }
+  validates :employee, :existence => true
+  
+  validates :leave_type_id, :uniqueness => { :scope => [:account_id, :employee_id, :date_as_at] }
+  validates :leave_type, :existence => true
+  
   validates :date_as_at, :timeliness => { :type => :date }
   validates :balance, :numericality => { :greater_than_or_equal_to => 0 }
 
