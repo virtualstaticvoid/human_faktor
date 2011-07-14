9ea78f345444df2126e00e2838a68947a743a9a6
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-14 13:44:40 +0200

    added after_create trap for leave request observer, for leave requests which auto confirm

--- app/models/leave_request_observer.rb
+++ app/models/leave_request_observer.rb
@@ -1,5 +1,9 @@
 class LeaveRequestObserver < ActiveRecord::Observer
 
+  def after_create(leave_request)
+    Resque.enqueue(LeaveRequestMailer, leave_request.id)
+  end
+
   def after_update(leave_request)
     Resque.enqueue(LeaveRequestMailer, leave_request.id)
   end
