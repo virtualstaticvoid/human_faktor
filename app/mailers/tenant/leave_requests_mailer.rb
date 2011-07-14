c3f4e7162f321c7e4d4d53c13a6c320bd0cbad08
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-14 12:21:46 +0200

    updated leave request mailer subject lines

--- app/mailers/tenant/leave_requests_mailer.rb
+++ app/mailers/tenant/leave_requests_mailer.rb
@@ -4,25 +4,25 @@ module Tenant
     def pending(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.approver.email, :subject => "#{@account.title} New Leave Request") if leave_request.approver.notify?
+      mail(:to => leave_request.approver.email, :subject => "#{AppConfig.title} - #{@account.title} - New Leave Request") if leave_request.approver.notify?
     end
 
     def approved(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.employee.email, :subject => "#{@account.title} Leave Approved") if leave_request.employee.notify?
+      mail(:to => leave_request.employee.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Approved") if leave_request.employee.notify?
     end
 
     def declined(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.employee.email, :subject => "#{@account.title} Leave Declined") if leave_request.employee.notify?  
+      mail(:to => leave_request.employee.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Declined") if leave_request.employee.notify?  
     end
 
     def cancelled(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.approver.email, :subject => "#{@account.title} Leave Cancelled") if leave_request.approver.notify?
+      mail(:to => leave_request.approver.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Cancelled") if leave_request.approver.notify?
     end
 
   end
--- test/functional/tenant/leave_requests_mailer_test.rb
+++ test/functional/tenant/leave_requests_mailer_test.rb
@@ -12,7 +12,7 @@ module Tenant
       @leave_request.confirm!
 
       mail = LeaveRequestsMailer.pending(@leave_request)
-      assert_equal "#{@account.title} New Leave Request", mail.subject
+      assert_equal "#{AppConfig.title} - #{@account.title} - New Leave Request", mail.subject
       assert_equal [@leave_request.approver.email], mail.to
       assert_equal [AppConfig.no_reply_email], mail.from
       #assert_match "Hi", mail.body.encoded
@@ -23,7 +23,7 @@ module Tenant
       @leave_request.approve!(@leave_request.approver, '')
 
       mail = LeaveRequestsMailer.approved(@leave_request)
-      assert_equal "#{@account.title} Leave Approved", mail.subject
+      assert_equal "#{AppConfig.title} - #{@account.title} - Leave Approved", mail.subject
       assert_equal [@leave_request.employee.email], mail.to
       assert_equal [AppConfig.no_reply_email], mail.from
       #assert_match "Hi", mail.body.encoded
@@ -34,7 +34,7 @@ module Tenant
       @leave_request.decline!(@leave_request.approver, '')
 
       mail = LeaveRequestsMailer.declined(@leave_request)
-      assert_equal "#{@account.title} Leave Declined", mail.subject
+      assert_equal "#{AppConfig.title} - #{@account.title} - Leave Declined", mail.subject
       assert_equal [@leave_request.employee.email], mail.to
       assert_equal [AppConfig.no_reply_email], mail.from
       #assert_match "Hi", mail.body.encoded
@@ -46,7 +46,7 @@ module Tenant
       @leave_request.cancel!(@leave_request.employee)
 
       mail = LeaveRequestsMailer.cancelled(@leave_request)
-      assert_equal "#{@account.title} Leave Cancelled", mail.subject
+      assert_equal "#{AppConfig.title} - #{@account.title} - Leave Cancelled", mail.subject
       assert_equal [@leave_request.approver.email], mail.to
       assert_equal [AppConfig.no_reply_email], mail.from
       #assert_match "Hi", mail.body.encoded
