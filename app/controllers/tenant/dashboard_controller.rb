12aafaa7dd94ea2569b363905884b8eed270ebf6
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-07-21 17:19:37 +0200

    refactored the Dashboard#balance controller action

--- app/controllers/tenant/dashboard_controller.rb
+++ app/controllers/tenant/dashboard_controller.rb
@@ -9,21 +9,23 @@ module Tenant
     end
 
     def balance
-      @date_as_at = ApplicationHelper.safe_parse_date(params[:as_at], Date.today)
-      @account = current_account
       @employee = params[:employee].present? ?
                     current_account.employees.find_by_identifier(params[:employee]) :
                     current_employee
+
+      # permission check
+      if @employee != current_employee && !current_employee.is_manager_of?(@employee) 
+        redirect_to dashboard_url and return false
+      end
+
+      @date_as_at = ApplicationHelper.safe_parse_date(params[:as_at], Date.today)
+      @account = current_account
     
       # get leave types, filtered by the gender of the employee
       @leave_types = current_account.leave_types.select {|leave_type| 
         !(leave_type.gender_filter & @employee.gender_filter).empty?
       }
-    
-      # permission check
-      if @employee != current_employee && !current_employee.is_manager_of?(@employee) 
-        redirect_to dashboard_url and return false
-      end
+      
     end
 
     def calendar
