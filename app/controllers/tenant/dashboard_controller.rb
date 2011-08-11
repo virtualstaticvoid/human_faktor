0d1d67fc840c2232d445781c7c1f1153df89d9eb
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-08-11 11:58:21 +0200

    added additional params

--- app/controllers/tenant/dashboard_controller.rb
+++ app/controllers/tenant/dashboard_controller.rb
@@ -37,8 +37,8 @@ module Tenant
       @staff_calendar = StaffCalendarEnquiry.new(current_account, current_employee).tap do |c|
         c.date_from = ApplicationHelper.safe_parse_date(staff_calendar_params[:date_from], Date.today << 3)
         c.date_to = ApplicationHelper.safe_parse_date(staff_calendar_params[:date_to], Date.today >> 9)
-        
-        # TODO: additional criteria
+        c.filter_by = staff_calendar_params[:filter_by] || 'none'
+        c.item_id = staff_calendar_params[:item_id]
         
         c.valid?
       end
