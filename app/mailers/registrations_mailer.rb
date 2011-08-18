882abc351e6e887660e7f32455fadf0436220531
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-08-18 11:09:38 +0200

    registration mailer blind copy to support email

--- app/mailers/registrations_mailer.rb
+++ app/mailers/registrations_mailer.rb
@@ -2,7 +2,11 @@ class RegistrationsMailer < BaseMailer
 
   def completed(registration)  
     @registration = registration
-    mail(:to => registration.email, :subject => "#{AppConfig.title} - Registration Completed")  
+    mail(
+      :to => registration.email, 
+      :bcc => AppConfig.support_email,
+      :subject => "#{AppConfig.title} - Registration Completed"
+    )  
   end  
 
 end
