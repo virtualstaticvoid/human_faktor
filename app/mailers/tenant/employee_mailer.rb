651ddb51deeec5bbc1fbff10eb7081993afdabe4
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-08-25 17:23:19 +0200

    need to ensure that the authentication token is generated

--- app/mailers/tenant/employee_mailer.rb
+++ app/mailers/tenant/employee_mailer.rb
@@ -4,6 +4,10 @@ module Tenant
     def activate(employee)
       @account = employee.account
       @employee = employee
+      
+      # ensure there the authentication token
+      @employee.ensure_authentication_token!
+      
       mail(
         :to => employee.email, 
         :subject => "#{AppConfig.title} - #{@account.title} - Employee Activation"
