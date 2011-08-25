1c91e894ef2eba41e3f25e1d39816e42604abf7f
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-08-25 16:16:20 +0200

    reformated code

--- app/mailers/tenant/leave_requests_mailer.rb
+++ app/mailers/tenant/leave_requests_mailer.rb
@@ -4,31 +4,46 @@ module Tenant
     def pending(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.approver.email, :subject => "#{AppConfig.title} - #{@account.title} - New Leave Request") if leave_request.approver.notify?
+      mail(
+        :to => leave_request.approver.email, 
+        :subject => "#{AppConfig.title} - #{@account.title} - New Leave Request"
+      ) if leave_request.approver.notify?
     end
 
     def approved(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.employee.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Approved") if leave_request.employee.notify?
+      mail(
+        :to => leave_request.employee.email, 
+        :subject => "#{AppConfig.title} - #{@account.title} - Leave Approved"
+      ) if leave_request.employee.notify?
     end
 
     def declined(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.employee.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Declined") if leave_request.employee.notify?  
+      mail(
+        :to => leave_request.employee.email, 
+        :subject => "#{AppConfig.title} - #{@account.title} - Leave Declined"
+      ) if leave_request.employee.notify?  
     end
 
     def cancelled(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.approver.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Cancelled") if leave_request.approver.notify?
+      mail(
+        :to => leave_request.approver.email, 
+        :subject => "#{AppConfig.title} - #{@account.title} - Leave Cancelled"
+      ) if leave_request.approver.notify?
     end
 
     def reinstated(leave_request)
       @account = leave_request.account
       @leave_request = leave_request
-      mail(:to => leave_request.employee.email, :subject => "#{AppConfig.title} - #{@account.title} - Leave Reinstated") if leave_request.employee.notify?
+      mail(
+        :to => leave_request.employee.email, 
+        :subject => "#{AppConfig.title} - #{@account.title} - Leave Reinstated"
+      ) if leave_request.employee.notify?
     end
 
   end
