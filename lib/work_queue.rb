5949147d1299cf0dbeebccc724a33438f7d4dd9f
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-09-02 13:20:59 +0200

    bug fix to job args

--- lib/work_queue.rb
+++ lib/work_queue.rb
@@ -4,7 +4,7 @@ module WorkQueue
 
   def self.enqueue(job_type, *args)
     # implemented via delayed_job (could also use Resque)
-    Delayed::Job.enqueue job_type.new(args)
+    Delayed::Job.enqueue job_type.new(*args)
   end
 
 end
