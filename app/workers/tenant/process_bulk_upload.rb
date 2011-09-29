efa6d393f0747f896d2a535be0f30802c755aa1e
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-09-29 16:49:25 +0200

    further work on bulk upload processor

--- app/workers/tenant/process_bulk_upload.rb
+++ app/workers/tenant/process_bulk_upload.rb
@@ -1,17 +1,76 @@
+require 'aws/s3'
+require 'csv'
+
 module Tenant
   class ProcessBulkUpload < Struct.new(:upload_id)
     
     def perform()
-      bulk_upload = BulkUpload.find(self.upload_id)
+      @bulk_upload = BulkUpload.find(self.upload_id)
 
-      # TODO:
-      #  1. stage
-      #  2. validate
-      #  3. apply
-      #  4. commit & update status
+      stage_upload() && validate_upload() && apply_upload() && complete_upload()
+        
+    rescue Exception => error
+    
+      # any staging, validation and apply 
+      # errors will be raised to here
+    
+      # log out the full error message
+      Rails.logger.error error.message
+      Rails.logger.error error.backtrace.join("\n")
 
+      # store the failure message
+      fail_upload(error.message)
+      
+      false
+    end
+    
+    private
+    
+    def stage_upload()
+    
+      # import the file as is into the bulk upload stage model
+      
+      #binding.pry
+      
+      options = { 
+        :header => :first_row, 
+        :return_headers => false,
+        :skip_blanks => true
+      }
+      
+      CSV.foreach(@bulk_upload.authenticated_url, 'r', *options) do |row|
+        # use row here...
+        
+        puts row.inspect
+        
+      end    
+    
+      false
+    end
 
-      false # for now...
+    def validate_upload()
+    
+      # validate each row, save the error message per row?
+      # and raise an exception at the end to indicate failure
+      # fault tolerant? skip problem rows?
+    
+      false
+    end
+
+    def apply_upload()
+    
+      # since approvers referenced may not exist, or are staged, 
+      # need to make a number of passes to solve any dependencies
+      
+      false
+    end
+
+    def complete_upload()
+      @bulk_upload.set_as_processed()
+    end
+
+    def fail_upload(error_message)
+      @bulk_upload.set_as_failed(error_message)
     end
 
   end
