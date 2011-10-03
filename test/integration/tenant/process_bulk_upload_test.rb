89e44babfefa39fa903058b45987b2317eec3d6f
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-03 19:29:01 +0200

    corrections to fixture and tests for bulk upload

--- test/integration/tenant/process_bulk_upload_test.rb
+++ test/integration/tenant/process_bulk_upload_test.rb
@@ -8,7 +8,7 @@ module Tenant
     test "should perform work" do
       
       bulk_upload = bulk_uploads(:one)
-      bulk_upload.csv_file = File.new(File.join(FIXTURES_DIR, 'bulk_upload.data'), 'r')
+      bulk_upload.csv = File.new(File.join(FIXTURES_DIR, 'bulk_upload.data'), 'r')
       assert bulk_upload.save
     
       result = ProcessBulkUpload.new(bulk_upload.id).perform()
