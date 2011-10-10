dedc98ecd4290eccda75f292d5e3d3f100f019d7
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-11 01:03:28 +0200

    removed logger settings from delayed job initializer

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,2 @@
 # for now, keep a history of jobs
 Delayed::Worker.destroy_failed_jobs = false
-
-# change the default logger
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', "#{Rails.env}_delayed_job.log"))
-
