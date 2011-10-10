4c5eccdfaa91d75c0e3efe8479226800e8319cc7
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-11 00:29:00 +0200

    improvements to unique user name validator

--- app/validators/unique_user_name_for_bulk_upload_validator.rb
+++ app/validators/unique_user_name_for_bulk_upload_validator.rb
@@ -1,8 +1,25 @@
 class UniqueUserNameForBulkUploadValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     
+    return if record.first_name.blank? || record.last_name.blank?
+    
     account = record.bulk_upload.account
-    record.errors[:email] << 'must be unique' if account.employees.where(:user_name => value).count() > 0
+    
+    # checks for duplicates in existing and proposed new employees
+    
+    if account.employees.where(
+        :user_name => value
+       ).count() > 0 || 
+       record.bulk_upload.records.where(
+         'LOWER(first_name) = :first_name AND LOWER(last_name) = :last_name',
+         { 
+          :first_name => record.first_name.downcase,
+          :last_name => record.last_name.downcase 
+         }
+       ).where("id <> #{record.id}").count() > 0
+    
+      record.errors[:user_name] << 'has already been taken'
+    end
     
   end
 end
