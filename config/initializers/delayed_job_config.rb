4f9f2cefe6fd9e57f2d7132ae040abbc91cd1a7a
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-13 13:02:20 +0200

    refactored delayed job initializer

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -5,6 +5,23 @@ Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.logger = ActiveSupport::BufferedLogger.new("log/#{Rails.env}_delayed_job.log", Rails.logger.level)
 Delayed::Worker.logger.auto_flushing = 1
 
-if caller.last =~ /.*\/script\/delayed_job:\d+$/
+#
+# check if run from $./script/delayed_job 
+#
+
+if caller.last =~ /^script\/delayed_job:.*$/
+
   ActiveRecord::Base.logger = Delayed::Worker.logger
+
+  #  
+  # Ensure all workers get required, so delayed_job doesn't silently fail when deserializing jobs
+  # See https://github.com/collectiveidea/delayed_job/wiki/Common-problems#wiki-jobs_are_silently_removed_from_the_database for details
+  #
+  
+  worker_path = File.expand_path('../../../app/workers', __FILE__)
+  Dir[File.join(worker_path, '**/**.rb')].each do |file|
+    puts ">>>> #{file}"
+    require file
+  end
+
 end
