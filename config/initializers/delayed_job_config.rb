9bc7e633104796acf174347bde45fa932d2c886a
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-13 11:54:29 +0200

    refactored delayed job logging configuration

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,10 @@
 # for now, keep a history of jobs
 Delayed::Worker.destroy_failed_jobs = false
 
-# change the default logger
-Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', "#{Rails.env}_delayed_job.log"))
+# configure logging
+Delayed::Worker.logger = ActiveSupport::BufferedLogger.new("log/#{Rails.env}_delayed_job.log", Rails.logger.level)
+Delayed::Worker.logger.auto_flushing = 1
 
+if caller.last =~ /.*\/script\/delayed_job:\d+$/
+  ActiveRecord::Base.logger = Delayed::Worker.logger
+end
