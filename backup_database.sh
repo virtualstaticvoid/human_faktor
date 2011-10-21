ff19295dbfc1f7185b8892ef1f24c12f0bd7bc4e
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-21 10:59:59 +0200

    revised database backup script added ignore for database backup files

--- backup_database.sh
+++ backup_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_development > hf_development_$(date +%Y%m%d)_$(date +%H%M).dump
-sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_production > hf_production_$(date +%Y%m%d)_$(date +%H%M).dump
-
+sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_test > db/backups/hf_test_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
+sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_development > db/backups/hf_development_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
+sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_production > db/backups/hf_production_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
