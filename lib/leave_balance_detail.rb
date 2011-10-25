a509a8d8903fe63115875ba0991388e48fc5c3b0
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-25 13:32:15 +0200

    added cycle start and end date fields to leave balance detail

--- lib/leave_balance_detail.rb
+++ lib/leave_balance_detail.rb
@@ -7,6 +7,8 @@ class LeaveBalanceDetail
   attr_reader :outstanding
   attr_reader :available
   attr_reader :projected
+  attr_reader :cycle_start_date
+  attr_reader :cycle_end_date
 
   def initialize(leave_type, employee, date_as_at)
     @take_on = leave_type.leave_take_on_for(employee, date_as_at)
@@ -16,6 +18,8 @@ class LeaveBalanceDetail
     @outstanding = leave_type.leave_outstanding_for(employee, date_as_at)
     @available = @take_on + @allowance - @leave_taken
     @projected = @available - @outstanding
+    @cycle_start_date = leave_type.cycle_start_date_of(employee, date_as_at)
+    @cycle_end_date = leave_type.cycle_end_date_of(employee, date_as_at)
   end
   
 end
