cf3121038d9ac686115c7860b87a4a7e2744d7b0
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-25 08:21:23 +0200

    updated tests for leave types

--- test/unit/leave_type_test.rb
+++ test/unit/leave_type_test.rb
@@ -5,6 +5,7 @@ class LeaveTypeTest < ActiveSupport::TestCase
   setup do
     @employee = employees(:employee)
     @employee.start_date = Date.new(2011, 1, 1)
+    @employee.take_on_balance_as_at = nil
   end
 
   test "fixture data valid" do
@@ -29,7 +30,11 @@ class LeaveTypeTest < ActiveSupport::TestCase
   end
   
   test "should get cycle index" do
-    leave_type = LeaveType.new(:cycle_start_date => Date.new(2011, 1, 1), :cycle_duration => 1)
+    leave_type = LeaveType.new(
+      :cycle_start_date => @employee.start_date, 
+      :cycle_duration => 1,
+      :cycle_duration_unit => LeaveType::DURATION_UNIT_YEARS
+    )
     assert_equal 0, leave_type.cycle_index_of(@employee, Date.new(2011, 1, 1))
     assert_equal 0, leave_type.cycle_index_of(@employee, Date.new(2011, 2, 1))
     assert_equal 0, leave_type.cycle_index_of(@employee, Date.new(2011, 12, 31))
@@ -37,21 +42,71 @@ class LeaveTypeTest < ActiveSupport::TestCase
     assert_equal 1, leave_type.cycle_index_of(@employee, Date.new(2012, 2, 1))
     assert_equal 1, leave_type.cycle_index_of(@employee, Date.new(2012, 12, 31))
     assert_equal 2, leave_type.cycle_index_of(@employee, Date.new(2013, 1, 1))
+    assert_equal 2, leave_type.cycle_index_of(@employee, Date.new(2013, 2, 1))
+    assert_equal 2, leave_type.cycle_index_of(@employee, Date.new(2013, 12, 31))
+    assert_equal 3, leave_type.cycle_index_of(@employee, Date.new(2014, 1, 1))
+    assert_equal 3, leave_type.cycle_index_of(@employee, Date.new(2014, 2, 1))
+    assert_equal 3, leave_type.cycle_index_of(@employee, Date.new(2014, 12, 31))
   end
 
-  test "should get cycle index over 20 of years" do
-    leave_type = LeaveType.new(:cycle_start_date => Date.new(2011, 1, 1), :cycle_duration => 1)
-    20.times do |i|
-      assert_equal i, leave_type.cycle_index_of(@employee, Date.new(2011 + i, 1, 1))
-      assert_equal i, leave_type.cycle_index_of(@employee, Date.new(2011 + i, 12, 31))
+  test "should get cycle index over 20 periods (years)" do
+    start_date = @employee.start_date
+    end_date = start_date + 1.year - 1.day
+    leave_type = LeaveType.new(
+      :cycle_start_date => start_date,
+      :cycle_duration => 1,
+      :cycle_duration_unit => LeaveType::DURATION_UNIT_YEARS
+    )
+    (0..19).each do |i|
+      assert_equal i, leave_type.cycle_index_of(@employee, start_date)
+      assert_equal i, leave_type.cycle_index_of(@employee, end_date)
+      start_date += 1.year
+      end_date += 1.year
     end
   end
   
-  test "should calculate cycle indexes, start and end dates" do
+  test "should get cycle index over 20 periods (months)" do
+    start_date = @employee.start_date
+    end_date = start_date + 1.month - 1.day
+    leave_type = LeaveType.new(
+      :cycle_start_date => start_date,
+      :cycle_duration => 1,
+      :cycle_duration_unit => LeaveType::DURATION_UNIT_MONTHS
+    )
+    (0..19).each do |i|
+      assert_equal i, leave_type.cycle_index_of(@employee, start_date)
+      assert_equal i, leave_type.cycle_index_of(@employee, end_date)
+      start_date += 1.month
+      end_date += 1.month
+    end
+  end
+
+  test "should get cycle index over 20 periods (days)" do
+    start_date = @employee.start_date
+    end_date = start_date + 29.days
+    leave_type = LeaveType.new(
+      :cycle_start_date => start_date,
+      :cycle_duration => 30,
+      :cycle_duration_unit => LeaveType::DURATION_UNIT_DAYS
+    )
+    (0..19).each do |i|
+      assert_equal i, leave_type.cycle_index_of(@employee, start_date)
+      assert_equal i, leave_type.cycle_index_of(@employee, end_date)
+      start_date += 30.days
+      end_date += 30.days
+    end
+  end
+
+  test "should calculate cycle indexes, start and end dates (employee-start-date)" do
     LeaveType.for_each_leave_type do |leave_type_class|
-      leave_type = leave_type_class.new()
-      leave_type.cycle_start_date = Date.new(2000, 1, 1)
-      @employee.start_date = Date.new(2000, 1, 1) unless leave_type.has_absolute_start_date?
+      @employee.start_date = Date.new(2000, 1, 1)
+      leave_type = leave_type_class.new(
+        :cycle_start_date => @employee.start_date,
+        :cycle_duration => 1,
+        :cycle_duration_unit => LeaveType::DURATION_UNIT_YEARS
+      )
+
+      next if leave_type.has_absolute_start_date?
 
       assert_equal 0, leave_type.cycle_index_of(@employee, Date.new(2000, 1, 1))
       assert_equal Date.new(2000, 1, 1), leave_type.cycle_start_date_for(@employee, 0)
@@ -67,16 +122,28 @@ class LeaveTypeTest < ActiveSupport::TestCase
     end
   end
 
+  test "should calculate cycle indexes, start and end dates (absolute-start-date)" do
+    LeaveType.for_each_leave_type do |leave_type_class|
+      leave_type = leave_type_class.new()
+
+      next unless leave_type.has_absolute_start_date?
+
+      # TODO
+
+    end
+  end
+
   test "should have zero allowance when date before start date of employee" do
     employee = employees(:employee)
     employee.start_date = Date.new(2000, 2, 1)
     employee.take_on_balance_as_at = nil
 
     LeaveType.for_each_leave_type do |leave_type_class|
-      #puts ">>> leave_type => #{leave_type_class}"
-      leave_type = leave_type_class.new()
-      leave_type.cycle_start_date = Date.new(2000, 1, 1)
-      leave_type.cycle_days_allowance = 1
+      leave_type = leave_type_class.new(
+        :cycle_start_date => Date.new(2000, 1, 1),
+        :cycle_duration => 1,
+        :cycle_duration_unit => LeaveType::DURATION_UNIT_YEARS
+      )
       
       assert_equal 0, leave_type.allowance_for(employee, Date.new(2000, 1, 1))
       assert_equal 0, leave_type.allowance_for(employee, Date.new(2000, 2, 1))
@@ -88,14 +155,15 @@ class LeaveTypeTest < ActiveSupport::TestCase
 
   test "should have zero allowance when date before balance take on date of employee" do
     employee = employees(:employee)
-    employee.start_date = nil
+    employee.start_date = Date.new(2000, 1, 1)
     employee.take_on_balance_as_at = Date.new(2000, 2, 1)
 
     LeaveType.for_each_leave_type do |leave_type_class|
-      #puts ">>> leave_type => #{leave_type_class}"
-      leave_type = leave_type_class.new()
-      leave_type.cycle_start_date = Date.new(2000, 1, 1)
-      leave_type.cycle_days_allowance = 1
+      leave_type = leave_type_class.new(
+        :cycle_start_date => Date.new(2000, 1, 1),
+        :cycle_duration => 1,
+        :cycle_duration_unit => LeaveType::DURATION_UNIT_YEARS
+      )
       
       assert_equal 0, leave_type.allowance_for(employee, Date.new(2000, 1, 1))
       assert_equal 0, leave_type.allowance_for(employee, Date.new(2000, 2, 1))
