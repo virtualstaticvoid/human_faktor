bc2729d5297c75571c650b51132ce087691319b7
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-25 12:01:04 +0200

    added pending tests

--- test/functional/tenant/tenant_controller_test.rb
+++ test/functional/tenant/tenant_controller_test.rb
@@ -4,7 +4,14 @@ module Tenant
   class TenantControllerTest < ActionController::TestCase
 
     # NOTE: controller is essentially abstract, although it imposes some before_filters
-    # not sure how exactly it can be tested
+
+    test "should check whether account is active" do
+      pending
+    end
+
+    test "should check whether employee has a password" do
+      pending
+    end
 
   end
 end
