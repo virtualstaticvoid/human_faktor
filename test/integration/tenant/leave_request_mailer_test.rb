344b0d759e587650124b26cf01b2917656f2b2de
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-10-25 12:00:57 +0200

    additional test case for leave request mailers

--- test/integration/tenant/leave_request_mailer_test.rb
+++ test/integration/tenant/leave_request_mailer_test.rb
@@ -5,9 +5,48 @@ module Tenant
   class LeaveRequestMailerTest < ActionDispatch::IntegrationTest
     fixtures :all
 
-    test "should perform work" do
+    test "should perform work when status pending" do
       leave_request = leave_requests(:one)
       leave_request.confirm!  # ensure confirmed
+
+      assert LeaveRequestMailer.new(leave_request.id).perform()
+      assert !ActionMailer::Base.deliveries.empty?
+    end
+
+    test "should perform work when status approved" do
+      leave_request = leave_requests(:one)
+      leave_request.confirm
+      leave_request.approve!(leave_request.approver, '')
+
+      assert LeaveRequestMailer.new(leave_request.id).perform()
+      assert !ActionMailer::Base.deliveries.empty?
+    end
+
+    test "should perform work when status declined" do
+      leave_request = leave_requests(:one)
+      leave_request.confirm
+      leave_request.decline!(leave_request.approver, '')
+
+      assert LeaveRequestMailer.new(leave_request.id).perform()
+      assert !ActionMailer::Base.deliveries.empty?
+    end
+
+    test "should perform work when status cancelled" do
+      leave_request = leave_requests(:one)
+      leave_request.confirm
+      leave_request.approve(leave_request.approver, '')
+      leave_request.cancel!(leave_request.approver)
+
+      assert LeaveRequestMailer.new(leave_request.id).perform()
+      assert !ActionMailer::Base.deliveries.empty?
+    end
+
+    test "should perform work when status reinstated" do
+      leave_request = leave_requests(:one)
+      leave_request.confirm
+      leave_request.approve(leave_request.approver, '')
+      leave_request.cancel(leave_request.approver)
+      leave_request.reinstate!(leave_request.approver)
 
       assert LeaveRequestMailer.new(leave_request.id).perform()
       assert !ActionMailer::Base.deliveries.empty?
