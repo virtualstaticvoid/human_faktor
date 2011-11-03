dd59510cad288811cab6beca971e383bb5a7f0e5
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-11-03 12:55:24 +0200

    moved scripts into correct folder revised backup script logic

--- backup_database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_test > db/backups/hf_test_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
-sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_development > db/backups/hf_development_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
-sudo -u postgres pg_dump -Fc --no-acl --no-owner hf_production > db/backups/hf_production_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
---
+++ script/backup_database.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo -u postgres pg_dump -Fc hf_test > db/backups/hf_test_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
+sudo -u postgres pg_dump -Fc hf_development > db/backups/hf_development_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
+sudo -u postgres pg_dump -Fc hf_production > db/backups/hf_production_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
