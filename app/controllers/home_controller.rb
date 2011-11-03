593e2aff891821bd3f37a8caf170bc71f87ca923
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-11-03 16:46:31 +0200

    added logic for determining the country from the request IP address

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'geoip'
+
 class HomeController < ApplicationController
 
   skip_before_filter :ensure_account
@@ -18,7 +20,30 @@ class HomeController < ApplicationController
   end
 
   def subscriptions
+    
+    @default_subscription = Subscription.default
     @subscriptions = Subscription.active.where('price > 0')
+
+    # use the country parameter
+    if params[:country].present?
+      @country = Country.by_iso_code(params[:country]) || Country.by_iso_code('za')
+    else
+
+      # see http://www.maxmind.com/app/country and http://www.maxmind.com/app/ruby for more details
+
+      # use Geo IP to figure out the country from the IP address
+      geoinf = GeoIP.new(File.join(Rails.root, 'db', 'geoip.dat')).country(request.remote_ip)
+
+      #
+      # For now, only interested in whether the request is from South Africa
+      #  otherwise, we use USA as the country
+      #
+      @country = (geoinf.country_code2 == "ZA") ? 
+                    Country.by_iso_code('za') :
+                    Country.by_iso_code('us') 
+
+    end
+
   end
 
   def partner
