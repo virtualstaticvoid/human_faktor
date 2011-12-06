063e69675d044adf963bdd78b3c6a86e303034c0
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-12-06 13:18:56 +0200

    revised logic for cycle end date

--- app/models/leave_type.rb
+++ app/models/leave_type.rb
@@ -141,7 +141,7 @@ class LeaveType < ActiveRecord::Base
   end
 
   def cycle_end_date_for(date_as_at, employee = nil)
-    cycle_start_date_for(date_as_at, employee) + cycle_duration_in_units - 1
+    self.cycle_start_date_for(date_as_at, employee) + cycle_duration_in_units - 1.day
   end
 
   def leave_carried_forward_for(employee, date_as_at)
