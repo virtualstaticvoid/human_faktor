615e7322f7a3e4646dc796590db18e4a24135b8f
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2012-10-15 21:08:45 +0200

    updated scripts etc

--- lib/date_filter.rb
+++ lib/date_filter.rb
@@ -2,15 +2,12 @@ class DateFilter
   include Informal::Model
 
   attr_accessor :date_from
-  attr_accessor :date_to
-
-  attr_accessor :date_from
   validates :date_from, :timeliness => { :type => :date }, :allow_nil => true
-  
+
   attr_accessor :date_to
   validates :date_to, :timeliness => { :type => :date }, :allow_nil => true
 
-  validate :date_from_must_occur_before_date_to, 
+  validate :date_from_must_occur_before_date_to,
            :date_to_must_occur_after_date_from
 
   def date_range
@@ -18,7 +15,7 @@ class DateFilter
   end
 
   private
-  
+
   def date_from_must_occur_before_date_to
     errors.add(:date_from, "can't be after the to date") if
       !(date_from.blank? || date_to.blank?) && (date_from > date_to)
--- script/backup_database.sh
+++ script/backup_database.sh
@@ -3,3 +3,4 @@
 sudo -u postgres pg_dump -Fc hf_test > db/backups/hf_test_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
 sudo -u postgres pg_dump -Fc hf_development > db/backups/hf_development_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
 sudo -u postgres pg_dump -Fc hf_production > db/backups/hf_production_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
+sudo -u postgres pg_dump -Fc hf_heroku_live > db/backups/hf_heroku_live_db_$(date +%Y%m%d)_$(date +%H%M)-`hostname`.backup
